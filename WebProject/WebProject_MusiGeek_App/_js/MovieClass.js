f96a9cb5c32e9919d963ed6c0dd181dd2c622177
NadinZeitoune <nadinz811@gmail.com>
2019-06-02 09:50:25 +0300

    movies DONe

--- WebProject/WebProject_MusiGeek_App/_js/MovieClass.js
+++ WebProject/WebProject_MusiGeek_App/_js/MovieClass.js
@@ -1,10 +1,11 @@
 class Movie{
-    constructor(_parent, _name, _prodaction, _img, _link){
+    constructor(_parent, _floatBox, _name, _prodaction, _img, _link){
         this.parent = _parent;
         this.name = _name;
         this.prodaction = _prodaction;
         this.img = _img;
         this.link = _link;
+        this.floatBox = _floatBox;
     }
 
     addToHtml(){
@@ -22,7 +23,16 @@ class Movie{
         newBox.onclick = function(){
             // Open floating scrren with the movie
             let embed = this.link.replace("watch?v=","embed/");
-            this.box.innerHTML += `<iframe width="560" height="315" src="${embed}" alt="sorry" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>`;
+            
+            this.floatBox.box.style.display = "flex";
+            this.floatBox.h2.innerHTML = this.name;
+            this.floatBox.box.style.textAlign = "center";
+            this.floatBox.iframe.innerHTML = `<iframe class="id_float_iframe" width="95%" height="450" src="${embed}" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope;picture-in-picture"></iframe>`;
+
+            // If there is no src- notify the user.
+            if(embed == ""){
+                this.floatBox.iframe.innerHTML = `<br><br><h3 class="text-warning">הסרט עדיין לא הגיע לאתר!</h3>`;
+            }
         }.bind(this);
     }
 }
--- WebProject/WebProject_MusiGeek_App/_js/movies.js
+++ WebProject/WebProject_MusiGeek_App/_js/movies.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    declareExitBtn();
 
     // Show the movies.
     doAjax("_json/musicals.json", function(_json_ar){
@@ -7,10 +8,16 @@ window.onload = function(){
                 prodaction,
                 poster,
                 movie} = item;
+            let floatBox = {box: id_float, h2: id_float_h2, iframe: id_iframe};
             
-            let myMovie = new Movie(id_parent, name, prodaction, poster, movie);
+            let myMovie = new Movie(id_parent, floatBox, name, prodaction, poster, movie);
             myMovie.addToHtml();
         }
     });
 }
 
+function declareExitBtn(){
+    id_float.addEventListener("click",function(){
+        id_float.style.display = "none";
+    })
+}
