993428f76c77dd04876f92522a8d921f31b88c66
NadinZeitoune <nadinz811@gmail.com>
2019-06-17 17:41:16 +0300

    div order

--- WebProject/WebProject_MusiGeek_App/_js/MusicalClass.js
+++ WebProject/WebProject_MusiGeek_App/_js/MusicalClass.js
@@ -8,26 +8,35 @@ class Musical {
 
     addToHtml() {
         let newBox = document.createElement("div");
-        newBox.className = "box col-md-5 col-12 border border-info p-3 mt-md-5 ";
+        newBox.className = "box col-md-5 col-12 border border-info p-3 mt-5 overflow-hidden";
         this.parent.appendChild(newBox);
 
-        newBox.innerHTML = `<img class="float-right ml-4" src="${this.img}" alt=${this.name}" poster">`;
-        newBox.innerHTML += `<h3 class="float-right">${this.name}</h3><br>`;
+        let div1 = document.createElement("div");
+        div1.className = "border border-warning col-8 overflow-hidden float-right";
+        newBox.appendChild(div1);
+        
+        div1.innerHTML = `<img class="float-right ml-4" src="${this.img}" alt=${this.name}" poster">`;
+        div1.innerHTML += `<h3 class="text-right">${this.name}</h3><br>`;
+
+        
+        let div2 = document.createElement("div");
+        div2.className = "border border-danger col-4 float-left";
+        newBox.appendChild(div2);
 
         let fav = document.createElement("i");
-        fav.className = "fa fa-heart fa-5x float-left border col-3";
+        fav.className = "fa fa-heart fa-5x";
         fav.style.color = "rgb(189, 189, 189)";
-        newBox.appendChild(fav);
+        div2.appendChild(fav);
 
         if (localStorage[`${this.name}`]) {
             fav.style.color = "red";
         }
 
-        newBox.onclick = function(){
-            newBox.style.backgroundColor = "red";
+        div1.onclick = function(){
+            div1.style.backgroundColor = "red";
         }.bind(this);
         
-        fav.onclick = function () {
+        div2.onclick = function () {
             if (localStorage[`${this.name}`]) {
                 fav.style.color = "rgb(189, 189, 189)";
                 localStorage.removeItem(this.name);
--- WebProject/WebProject_MusiGeek_App/_js/musicals.js
+++ WebProject/WebProject_MusiGeek_App/_js/musicals.js
@@ -1,5 +1,7 @@
 window.onload = function(){
     let newmusic = new Musical(id_parent, id_extansion, "שתי טיפות של דם", "_images/andTheSushiFree.jpg");
     newmusic.addToHtml();
+    let music = new Musical(id_parent, id_extansion, "שתי טיפות של דם", "_images/andTheSushiFree.jpg");
+    music.addToHtml();
 }
 
